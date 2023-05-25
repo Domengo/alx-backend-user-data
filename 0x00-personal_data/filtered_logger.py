1c1e4e8df6e8e667e4b019ed02eed4c28ae4b2b0
Domengo <dominicsengo@gmail.com>
2023-05-25 18:58:52 +0300

    regexing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,5 +6,6 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    regex = re.compile(r'(?<=^|{})([^{}]+)(?={}|$)'.format(separator, separator))
-    return re.sub('|'.join(fields), redaction, message, flags=re.IGNORECASE)
+    regex = re.compile(r'(?<=^|{})([^{}]+)(?={}|$)'.format(re.escape(separator), re.escape(separator)))
+    return regex.sub(redaction, message)
+
