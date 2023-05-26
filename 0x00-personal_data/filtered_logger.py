51c98f08f47950a35bb3e61020c619742bec83e1
Domengo <dominicsengo@mail.com>
2023-05-26 14:04:47 +0300

    regexing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,4 +8,3 @@ import re
 def filter_datum(fields, redaction, message, separator):
     regex = re.compile(r'(?<=^|{})([^{}]+)(?={}|$)'.format(re.escape(separator), re.escape(separator)))
     return regex.sub(redaction, message)
-
