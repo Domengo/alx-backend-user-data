40d4ef4c36a4cee5e150b1f015c5b7b5445cb74b
Domengo <dominicsengo@mail.com>
2023-06-02 18:41:53 +0300

    unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route("/api/v1/unauthorized")
-def unauthorized():
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def authorized() -> str:
     """Raises a 401 error."""
-    abort(401)
+    abort(401, description="Unauthorized")
