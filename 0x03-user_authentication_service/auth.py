fa9c86ee8226dccea5a022800abf6e2225945843
Domengo <dominicsengo@mail.com>
2023-06-09 17:44:23 +0300

    generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,7 +63,7 @@ class Auth:
         return bcrypt.checkpw(password.encode('utf-8'),
                               user.hashed_password)
 
-    def _generate_uuid(self) -> str:
+    def _generate_uuid() -> str:
         """_summary_
 
         Returns:
