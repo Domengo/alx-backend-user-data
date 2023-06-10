a01c1cd08ecc611d9ad92c6697f66b4ee083b305
Domengo <dominicsengo@mail.com>
2023-06-10 13:15:34 +0300

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -81,7 +81,7 @@ def profile() -> str:
         abort(403)
 
 
-@route('/reset_password', methods=['POST'], strict_slashes=False)
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token() -> str:
     """POST /reset_password
     Return:
